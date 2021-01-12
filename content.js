89637de91f4942c9cc936cf257f13677ae2ca43e
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-12 21:38:11 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,9 @@
+const secondary = document.getElementById("secondary");
+const meta = document.getElementById("meta");
+const comments = document.getElementById("comments");
+const info = document.querySelectorAll('#info')[3]
+
+secondary.style.display = "none";
+meta.style.display = "none";
+comments.style.display = "none";
+info.style.display = "none";
